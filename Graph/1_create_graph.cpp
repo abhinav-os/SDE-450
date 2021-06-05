1a065899b4ab84937bd9e74f050a8daa55f3cf98
Abhinav <abhinavkiran73@gmail.com>
2021-06-06 00:52:35 +0530

    Finished 1_create_graph

--- Graph/1_create_graph.cpp
+++ Graph/1_create_graph.cpp
@@ -1,18 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
+void adjacency_matrix() {
+    int n, m;
+    cin >> n >> m;
 
-    int sum = 0;
-    for(int i = 0; i < n; i++)
-        sum += a[i];
+    int mat[n+1][m+1];
+    for(int i = 0; i < m; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        mat[u][v] = wt;
+        mat[u][v] = wt;
+    }
+}
+
+void adjacency_list() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> adj[n+1];
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+}
+
+void weighted_adjacency_list() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<int, int> > adj[n+1];
+
+    for(int i = 0; i < m; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
+    }
+
     
-    cout << sum << "\n"; 
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < adj[i].size(); j++) {
+            cout << "(" << adj[i][j].first << "," << adj[i][j].second << ")" << "; ";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
@@ -24,6 +59,19 @@ int main() {
     int t;
     cin >> t;
     while(t--) {
-        solve();
+        // adjacency_matrix();
+        // adjacency_list();
+        weighted_adjacency_list();
     }
 }
+
+/*
+1       //  Test Cases
+5 6     // Number of nodes, number of edges, next 6 lines (u,v) describe that there is an edge between u,v
+1 2 2   // (u, v, weight)
+1 3 3
+3 4 4
+2 4 5
+3 5 4 
+4 5 2
+*/
