61183df3c466ee3b822ff96d579da5a546dbfdae
Abhinav <abhinavkiran73@gmail.com>
2021-06-29 19:22:27 +0530

    Implemeted 7_cyclically_rotate_an_array

---
+++ Array/7_cyclically_rotate_an_array.cpp
@@ -0,0 +1,45 @@
+/*
+    Time Complexity: theta(n)
+    Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate_left(vector<int> a, int k) {
+    rotate(a.begin(), a.begin() + k, a.end());
+    for(auto ele : a) {
+        cout << ele << " ";
+    }
+    cout << endl;
+}
+
+void rotate_right(vector<int> a, int k) {
+    rotate(a.begin(), a.begin() + (a.size() - k), a.end());
+    for(auto ele : a) {
+        cout << ele << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    rotate_left(a, 3);
+    rotate_right(a, 3);
+}
+
+/*
+9
+1 2 3 4 5 6 7 8 9
+*/
