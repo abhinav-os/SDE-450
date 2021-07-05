b0cea8bbb137d0f6f39a65d511ca090d2f630589
Abhinav <abhinavkiran73@gmail.com>
2021-07-06 00:06:55 +0530

    Implemeted 10_minimum_number_of_jumps

--- Array/10_minimum_number_of_jumps.cpp
+++ Array/10_minimum_number_of_jumps.cpp
@@ -1,10 +1,50 @@
 /*
-    Time Complexity: 
-    Space Complexity: 
+    Time Complexity: O(N)
+    Space Complexity: O(1)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+/**
+ * maxReach: At all times contains maximum reachable index, initialized to a[0];
+ * jumps: Amount of jump required to reach that maximum reachabale index, initialized to 1;
+ * steps: Maximum step we can still take,  initialized to a[0]
+ * 
+ * Iterate from index 1: Check if we reached end of the array, if yes, return jumps
+ * Else, update maxReach := max(maxReach, i + a[i]); steps := steps - 1
+ * Check if steps is 0: If yes, we need another jump (jumps := jumps + 1); Update steps
+ */ 
+
+int minJumps(vector<int> a) {
+    int n = a.size();
+    
+    if(n == 1) {
+        return 0;
+    }
+
+    if(a[0] == 0) {
+        return -1;
+    }
+
+    int maxReach = a[0], jumps = 1, steps = a[0];
+    for(int i = 1; i < n; i++) {
+        if(i == n-1) {
+            return jumps;
+        }
+
+        maxReach = max(maxReach, i + a[i]);
+        steps -= 1;
+
+        if(steps == 0) {
+            jumps += 1;
+            if(i >= maxReach) {
+                return -1;
+            }
+            steps = maxReach - i;
+        }
+    }
+}
 
 /**
  * DP Approach
@@ -37,11 +77,6 @@ int minJumps(vector<int> a) {
     return (dp[n-1] == INT_MAX ? -1 : dp[n-1]);
 }
 */
-
-int minJumps(vector<int> a) {
-    int n = a.size();
-    
-}
 
 int main() {
     #ifndef ONLINE_JUDGE
