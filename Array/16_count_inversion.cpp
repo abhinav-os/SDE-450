cbeb84c66f0568ad3ee11165ccdc24c65a52d7a8
Abhinav <abhinavkiran73@gmail.com>
2021-07-05 22:38:07 +0530

    Implemnted 16_count_inversion

--- Array/16_count_inversion.cpp
+++ Array/16_count_inversion.cpp
@@ -1,35 +1,65 @@
 /*
-    Time Complexity: 
-    Space Complexity: 
+    Time Complexity: O(N*logN)
+    Space Complexity: O(N)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int countInversion(vector<int>& a) {
-    int n = a.size();
+void merge(vector<int>& a, int left, int mid, int right, int& ans) {
+    // [left,..,mid] [mid+1,...,right]
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-    vector<pair<int, int>> v(n);
-    for(int i = 0; i < n; i++) {
-        v[i] = {a[i], i};
+    vector<int> temp1(n1);
+    vector<int> temp2(n2);
+
+    int i, j, k;
+
+    for(i = 0; i < n1; i++) {
+        temp1[i] = a[i + left];
     }
 
-    sort(v.begin(), v.end());
+    for(i = 0; i < n2; i++) {
+        temp2[i] = a[i + mid + 1];
+    }
+    
+    i = 0, j = 0, k = left;
 
-    vector<bool> vis(n, false);
-    int ans = 0;
-    for(int i = 0; i < n; i++) {
-        if(!vis[i] && v[i].first != a[i]) {
-            int cnt = 0, j = i;
-            while(!vis[j]) {
-                cnt += 1;
-                vis[j] = true;
-                j = v[j].second;
-            }
-            if(cnt > 0)
-                ans += (cnt-1);
+    while(i < n1 && j < n2) {
+        if(temp1[i] <= temp2[j]) {
+            a[k++] = temp1[i++];
+        } else {
+            a[k++] = temp2[j++]; 
+            ans += (n1-i);
         }
     }
+
+    while(i < n1) {
+        a[k++] = temp1[i++];
+    }
+
+    while(j < n2) {
+        a[k++] = temp2[j++];
+    }
+}
+
+void mergeSortUtil(vector<int>& a, int left, int right, int& ans) {
+    if(left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSortUtil(a, left, mid, ans);
+        mergeSortUtil(a, mid+1, right, ans);
+
+        merge(a, left, mid, right, ans);
+    }
+}
+
+int inversionCount(vector<int>& a) {
+    int left = 0, right = a.size() - 1;
+    int ans = 0;
+
+    mergeSortUtil(a, left, right, ans);
 
     return ans;
 }
@@ -47,7 +77,13 @@ int main() {
         cin >> a[i];
     } 
 
-    cout << countInversion(a);
+    cout << inversionCount(a) << "\n";
+
+    /*
+    for(auto ele : a) {
+        cout << ele << " ";
+    }
+    */
 }
 
 /*
