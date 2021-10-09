99d3b3e714e34cefdd61e346c0fe2f9022a96c51
Abhinav <abhinavkiran73@gmail.com>
2021-10-09 15:56:11 +0530

    Implemented 2_knapsack_problem

---
+++ Dynamic Programming/2_knapsack_problem.cpp
@@ -0,0 +1,50 @@
+/*
+    Time Complexity: O(N * W)
+    Space Complexity: O(N * W)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapsack(vector<int>& wt, vector<int>& val, int w, int n)
+{
+    vector<vector<int>> dp(n + 1, vector<int>(w+1));
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= w; j++) {
+            if(i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= w; j++) {
+            if(wt[i-1] <= j) 
+                dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]],  dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    return dp[n][w];
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, w;
+    cin >> n >> w;
+    vector<int> wt(n), val(n);
+    for(int i = 0; i < n; i++)      cin >> wt[i];
+    for(int i = 0; i < n; i++)      cin >> val[i];
+
+    cout << knapsack(wt, val, w, n);   
+}
+
+/*
+3 4
+1 2 3
+4 5 1
+*/
