ec73f6ebe7e22bba2a0f9a0c547d7479e27bbd8c
Abhinav <abhinavkiran73@gmail.com>
2022-01-23 00:48:08 +0530

    Implemented 06_remove_duplicate_element_from_sorted_linked_list

---
+++ Linked List/06_remove_duplicate_element_from_sorted_linked_list.cpp
@@ -0,0 +1,86 @@
+/*
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *newNode(int data)
+{
+    Node *temp = new Node();
+    temp->data = data;
+    temp->next = NULL;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+Node *removeDuplicates(Node *head)
+{
+    Node *newHead = NULL;
+    while (head != NULL && head->next != NULL && head->data == head->next->data)
+        head = head->next;
+
+    newHead = head;
+    Node *prev = head;
+    head = head->next;
+
+    while (head != NULL && head->next != NULL)
+    {
+        if (head->data == head->next->data)
+            head = head->next;
+        else
+        {
+            prev->next = head;
+            prev = head;
+            head = head->next;
+        }
+    }
+
+    prev->next = head;
+
+    return newHead;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n, num;
+    cin >> n >> num;
+
+    Node *head, *tail;
+    head = tail = newNode(num);
+
+    for (int i = 1; i < n; i++)
+    {
+        cin >> num;
+        tail->next = newNode(num);
+        tail = tail->next;
+    }
+
+    print(head);
+    Node *newHead = removeDuplicates(head);
+    print(newHead);
+}
+
+/*
+
+*/
